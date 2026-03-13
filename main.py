55dbee88f3bb5f85cd574612ad20488d7e928b8e
azerly3 <azerly3@gmail.com>
2026-03-13 22:03:37 +0100

    Added delta time, working

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def main():
     )
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     number = 5
     while number != 0:
         log_state()
@@ -20,6 +22,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+
     
 
 if __name__ == "__main__":
